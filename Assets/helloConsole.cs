17e0781b1efb1db3c67cf9eb4d7bed98e746209d
mumblespolecat <2031528@swamsea.ac.uk>
2020-10-11 20:47:45 +0100

    Revert "Commit after a deletion (culled a chicken)"
    This reverts commit e5db334bc42b22d55ea9951c96e76f9945452f4f.

--- Assets/helloConsole.cs
+++ Assets/helloConsole.cs
@@ -7,6 +7,6 @@ public class helloConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Matthew");
+        Debug.Log("Hello Console");
     }
 }
